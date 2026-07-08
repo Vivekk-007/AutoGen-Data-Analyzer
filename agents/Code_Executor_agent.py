bbeb56e5df32db88eba86b6e638f880f583a3891
vivekkk-007 <kumarvivek05093896@gmail.com>
2026-07-08 11:10:52 +0530

    Initial project setup

---
+++ agents/Code_Executor_agent.py
@@ -0,0 +1,54 @@
+from autogen_agentchat.agents import CodeExecutorAgent
+import asyncio
+from autogen_agentchat.messages import TextMessage
+from autogen_core import CancellationToken
+from autogen_ext.code_executors.docker import DockerCommandLineCodeExecutor
+
+
+def getCodeExecutorAgent(code_executor):
+
+    code_executor_agent = CodeExecutorAgent(
+        name='CodeExecutor',
+        code_executor = code_executor
+    )
+
+    return code_executor_agent
+
+
+async def main():
+
+    docker=DockerCommandLineCodeExecutor(
+    work_dir='temp',
+    timeout=120
+)
+
+
+    code_executor_agent = getCodeExecutorAgent(docker)
+
+    task = TextMessage(
+        content=''' Here is the Python Code which You have to run.
+```python
+print('Hello Wooooooooorld')````        
+''',
+    source='User'
+    )
+
+
+    try:
+        await docker.start()
+
+        res = await code_executor_agent.on_messages(
+            messages=[task],
+            cancellation_token=CancellationToken()
+        )
+        print('result is :',res)
+
+    except Exception as e:
+        print(e)
+    finally:
+        await docker.stop()
+
+if (__name__ == '__main__'):
+    asyncio.run(main())
+
+    
